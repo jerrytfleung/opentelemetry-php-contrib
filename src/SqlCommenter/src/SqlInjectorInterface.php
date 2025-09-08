3c370b020a46bf99b627254c64f39eca58b01e64
Jerry Leung <tingfung.leung@solarwinds.com>
2025-09-08 11:53:13 -0700

    Initial

---
+++ src/SqlCommenter/src/SqlInjectorInterface.php
@@ -0,0 +1,10 @@
+<?php
+//
+//declare(strict_types=1);
+//
+//namespace OpenTelemetry\Contrib\SqlCommenter;
+//
+//interface SqlInjectorInterface
+//{
+//    public static function inject(string $query, array $comments): string;
+//}
